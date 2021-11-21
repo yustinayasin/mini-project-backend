2a427615697f2d6d0636fac4fd786f6372209a8e
yustinayasin <yustinayasin@gmail.com>
2021-11-21 23:21:06 +0700

    feat: add new routes for table kemeja, keranjang, and kemeja keranjang

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	kemejaKeranjangController "kemejaku/controllers/kemejakeranjangs"
+	kemejaController "kemejaku/controllers/kemejas"
+	keranjangController "kemejaku/controllers/keranjangs"
 	userController "kemejaku/controllers/users"
 
 	"github.com/labstack/echo/v4"
@@ -8,11 +11,15 @@ import (
 )
 
 type RouteControllerList struct {
-	UserController userController.UserController
+	UserController            userController.UserController
+	KeranjangController       keranjangController.KeranjangController
+	KemejaController          kemejaController.KemejaController
+	KemejaKeranjangController kemejaKeranjangController.KemejaKeranjangController
 	// JWTConfig      middleware.JWTConfig
 }
 
 func (controller RouteControllerList) RouteRegister(e *echo.Echo) {
+
 	eUser := e.Group("/user")
 	// eUser.Use(middleware.JWTWithConfig(controller.JWTConfig))
 	e.POST("/login", controller.UserController.Login)
@@ -23,4 +30,28 @@ func (controller RouteControllerList) RouteRegister(e *echo.Echo) {
 	eUser.GET("/:userId", controller.UserController.GetUserDetail)
 	e.GET("/users", controller.UserController.GetAllUsers)
 	// users.GET("/", controller.UserController.Login, middleware.JWTWithConfig(controller.JWTConfig))
+
+	eKeranjang := e.Group("/keranjang")
+
+	eKeranjang.GET("/:keranjangId", controller.KeranjangController.GetKeranjangDetail)
+	e.GET("/keranjangs", controller.KeranjangController.GetAllKeranjang)
+	e.POST("/keranjangs", controller.KeranjangController.InsertKeranjang)
+	eKeranjang.PUT("/:keranjangId", controller.KeranjangController.EditKeranjang)
+	eKeranjang.DELETE("/:keranjangId", controller.KeranjangController.DeleteKeranjang)
+
+	eKemejaKeranjang := e.Group("/kemejakeranjang")
+
+	eKemejaKeranjang.GET("/:kemejaKeranjangId", controller.KemejaKeranjangController.GetKemejaKeranjangDetail)
+	e.GET("/kemejakeranjangs", controller.KemejaKeranjangController.GetAllKemejaKeranjang)
+	e.POST("/kemejakeranjangs", controller.KemejaKeranjangController.InsertKemejaKeranjang)
+	eKemejaKeranjang.PUT("/:kemejaKeranjangId", controller.KemejaKeranjangController.EditKemejaKeranjang)
+	eKemejaKeranjang.DELETE("/:kemejaKeranjangId", controller.KemejaKeranjangController.DeleteKemejaKeranjang)
+
+	eKemeja := e.Group("/kemeja")
+
+	eKemeja.GET("/:kemejaKeranjangId", controller.KemejaController.GetKemejaDetail)
+	e.GET("/kemejakeranjangs", controller.KemejaController.GetAllKemeja)
+	e.POST("/kemejakeranjangs", controller.KemejaController.InsertKemeja)
+	eKemeja.PUT("/:kemejaKeranjangId", controller.KemejaController.EditKemeja)
+	eKemeja.DELETE("/:kemejaKeranjangId", controller.KemejaController.DeleteKemeja)
 }
